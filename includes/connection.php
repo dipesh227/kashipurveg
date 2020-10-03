1f2eccd600277ac75330f07c16e0dcbb2c60280b
Dipesh Joshi <54115006+dipesh227@users.noreply.github.com>
2020-10-03 22:17:25 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
 <?php 
-$con=mysqli_connect("http://149.129.179.5:888/","root","dipesh@90","kashipurvegadda");
+$con=mysqli_connect("http://149.129.179.5:888/phpmyadmin_3cb49bf35a357ee9/db_structure.php","root","dipesh@90","kashipurvegadda");
 //$con=mysqli_connect("bymn12ac9v3dy9rblgoi-mysql.services.clever-cloud.com","uwb5jr3dsdzm2ole","mnVyapBrcKy3wH767Lp7","bymn12ac9v3dy9rblgoi");
 ?>
