a652a3e19d6b03c5afc09daca0f9708be480f31a
Dipesh Joshi <54115006+dipesh227@users.noreply.github.com>
2020-10-03 22:13:04 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,4 @@
 <?php 
-$con=mysqli_connect("webhosting","root","dipesh@90","kashipurvegadda");
+$con=mysqli_connect("149.129.179.5","root","dipesh@90","kashipurvegadda");
 //$con=mysqli_connect("bymn12ac9v3dy9rblgoi-mysql.services.clever-cloud.com","uwb5jr3dsdzm2ole","mnVyapBrcKy3wH767Lp7","bymn12ac9v3dy9rblgoi");
 ?>
