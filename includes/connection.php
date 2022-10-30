746bd0e1c7bd27314022acf4ceda7b18e3930a48
Dipesh Joshi <54115006+dipesh227@users.noreply.github.com>
2022-10-30 19:12:02 +0530

    database

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php 
-$con=mysqli_connect("http://149.129.179.5:888/phpmyadmin_3cb49bf35a357ee9/index.php","root","dipesh@90","kashipurvegadda");
+// $con=mysqli_connect("http://149.129.179.5:888/phpmyadmin_3cb49bf35a357ee9/index.php","root","dipesh@90","kashipurvegadda");
 // if($con){echo 'yes';}else{echo'no'}
-//$con=mysqli_connect("bymn12ac9v3dy9rblgoi-mysql.services.clever-cloud.com","uwb5jr3dsdzm2ole","mnVyapBrcKy3wH767Lp7","bymn12ac9v3dy9rblgoi");
+$con=mysqli_connect("bymn12ac9v3dy9rblgoi-mysql.services.clever-cloud.com","bybwrw6uurgujryuwdyz","mnVyapBrcKy3wH767Lp7","bymn12ac9v3dy9rblgoi");
 ?>
